6f9c2760c9682eb475727575a0d30b300a6fa1f8
Mayeul CROGUENNEC <ma.croguennec@orange.fr>
2021-01-14 11:10:32 +0100

    Footer complete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,31 @@
 <template>
  <footer>
-   <p>Wesh la street</p>
+   <div class="news_bottom">
+       <p id="gauche">@ 2020 - Mayeul Croguennec</p>
+       <p id="milieu_gauche">Mentions légales</p>
+       <p id="milieu_droite">Mon profil</p>
+       <p id="droite">Mes projets</p>
+   </div>
  </footer>
 </template>
 
 
 
 <style scoped>
+footer{
+  background: none;
+  height: 40px;
+}
+p {
+  font-family: "roboto-regular";
+  font-size: 14px;
+  color: var(--secondary-color);
+}
 
-</style>
+#gauche, #milieu_gauche, #milieu_droite, #droite {
+  margin-top: 12px;
+  display: inline-block;
+  vertical-align: top;
+  width: 23%;
+}
+</style>s
